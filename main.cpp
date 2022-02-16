7d4039a6de99c7b27485a840eace6b7404be30d0
Aaron Speedy <Aaron-Speedy>
2022-02-16 10:17:52 -0600

    command line arguments

--- main.cpp
+++ main.cpp
@@ -3,9 +3,6 @@
 #include <string>
 #include <fstream>
 #include <streambuf>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/syscall.h>
 #include "headers/program.h"
 
 int main(int argc,  char **argv) {
